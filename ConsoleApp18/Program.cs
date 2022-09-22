5c92ac7f7205132e8f5b7e8444f65eb36c2972d7
Rahuldal123 <rahuldalwale55@gmail.com>
2022-09-22 13:54:24 +0530

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // WAP to find given year is leap or not
+
+            //Console.WriteLine("Enter a number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //if (num % 4 == 0)
+            //{
+            //    Console.WriteLine("Leap year");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not leap year");
+            //}
+
+            //switch case
+
+
+            //Console.WriteLine("1. for Eng");
+            //Console.WriteLine("2. for Hindi");
+            //Console.WriteLine("3. for Marathi");
+            //Console.WriteLine("select your option");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //switch (num)
+            //{
+            //    case 1:
+            //        Console.WriteLine("You have selected Eng lang");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("You have selected Hindi lang");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("You have selected Marathi lang");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Wrong input");
+            //        break;
+            //}
+
+
+
+            // WAp to accept two numbers from user and ask  for operation
+
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Press 1. for addision" +
+            //    "Press 2. for substraction" +
+            //    "Press 3. for multiplication" +
+            //    "Press 4. for division" +
+            //    "Press 5.for mod");
+            //int num=Convert.ToInt32(Console.ReadLine());
+
+            //switch (num)
+            //{
+            //    case 1:
+            //Console.WriteLine($"addition is{a + b}");
+            //        break;
+            //    case 2:
+            //Console.WriteLine($"substraction is{a - b}");
+            //        break;
+
+            //    case 3:
+            //        Console.WriteLine($"multiplication is{a * b}");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine($"division is {a / b}");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine($"mod is{a%b}");
+            //        break;
+            //        default:
+            //        Console.WriteLine("wrong input");
+            //        break;
+
+
+            // print fibonacci series using while loop
+
+            //int n1 = 0, n2 = 1, n3;
+            //int i = 2;
+            //Console.WriteLine("enter upper limit");
+            //int num=Convert.ToInt32(Console.ReadLine());
+            //Console.Write($"{n1} {n2}");
+            //while(i <num)
+            //{
+            //    n3=n1+ n2;
+            //    Console.Write($"{n3}");
+            //    n1 = n2;
+            //    n2 = n3;
+
+            //    i++;
+            //}
+
+            // print numbers from 1 to 50 using while loop
+
+            //int j = 1;
+            //while(j<=50)
+            //{
+            //    Console.WriteLine(j);
+            //    j++;
+            //}
+
+            //print even numbers from 1 to 20
+
+            //int i = 1;
+            //while(i<=20)
+            //{
+            //    if(i%2==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //    i++;   
+
+            //}
+
+            // print odd numbers from 100 to 50 in revers order
+
+            //int i = 100;
+            //while(i>50)
+            //{
+            //    if(i%2!=0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i--;
+            //}
+
+            //accept number from user and print table
+
+            //int a=Convert.ToInt32(Console.ReadLine());
+            //int i = 1;
+            //int b = 0;
+            //while(i<=10)
+            //{
+            //    b = b + a;
+            //    Console.WriteLine(b);
+            //    i++;
+            //}
+
+            // wap to print numbers from 1 to 100 which are divisible by 5
+
+            //int i = 1;
+            //while(i<=100)
+            //{
+            //    if(i%5==0)
+            //    {
+            //        Console.WriteLine(i);
+
+            //    }
+            //    i++;
+            //}
+
+            // accept the number from user and find factorial
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int i = 1;
+            //int fact = 1;
+            //while(i<=a)
+            //{
+            //    fact = fact * i;
+
+            //    i++;
+            //}
+            //Console.WriteLine(fact);
+
+            // do while loop 
+
+            // find fibo series of given number do while loop
+
+
+            //int n, n1 = 0, n2 = 1, n3;
+            //int i = 2;
+            //Console.WriteLine("Enter a upper limit");
+            //n = Convert.ToInt32(Console.ReadLine());
+            //Console.Write($"{n1}  {n2}");
+            //do
+            //{
+            //    n3 = n1 + n2;
+            //    Console.Write($" {n3}");
+            //    n1 = n2;
+            //    n2 = n3;
+
+            //    i++;
+            //} while (i < n);
+
+            // print numbers from 1 to 50 using  do while loop
+
+            //int i = 0;
+            //do
+            //{
+            //    i++;
+            //    Console.WriteLine(i);
+            //}
+            //while (i < 50);
+
+            //print even numbers from 1 to 20
+
+            //int i = 0;
+            //do
+            //{
+            //    i++;
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //while (i <= 20);
+
+            // print odd numbers from 100 to 50 in revers order using do while loop
+
+            //int i = 100;
+
+            //do
+            //{
+            //    i--;
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //while (i > 50);
+
+
+            //accept number from user and print table
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int i = 1;
+            //int b = 0;
+            //do
+            //{
+            //    i++;
+            //    b = b + a;
+            //    Console.WriteLine(b);
+            //}
+            //while (i <= 10);
+
+            //wap to print numbers from 1 to 100 which are divisible by 5
+
+            //int i = 1;
+            //do
+            //{
+            //    i++;
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //while (i <= 100);
+
+
+        }
+
+    }
+
+    
+}
+
